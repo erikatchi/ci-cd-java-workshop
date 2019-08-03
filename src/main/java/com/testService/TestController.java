3b7a1e1a52877b5d8ebb8a57d32cf6bd0896a904
Erika Taniguchi <erikatchi@hotmail.com>
2019-08-03 14:37:12 -0300

    Correção dos erros de formatação apontados pelo Checkstyle e FingBugs

--- src/main/java/com/testService/TestController.java
+++ src/main/java/com/testService/TestController.java
@@ -4,12 +4,20 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/** Exemplo básico de um comentário em JavaDoc.
+ * com mais de uma linha.
+ */
+
 @RestController
 @CrossOrigin
-public class TestController {
-
-	@GetMapping("/home")
-	public String homeRequest() {
-		return "Hello World!";
-	}
+    public class TestController {
+     /** Exemplo básico de um comentário em JavaDoc.
+      * com mais de uma linha.
+      */
+      /** @return result of operation if log successful. */
+         @GetMapping("/home")
+         public String homeRequest() {
+           /** Hello World */
+           return "Hello World!";
+      }
 }
---
+++ src/main/java/com/testService/package-info.java
@@ -0,0 +1,11 @@
+/**Teste Controller - Workshowp.
+ * @author Erika Taniguchi.
+ * @version 1.05.
+ * @since Release 01 da aplicação.
+ */
+
+package com.testService;
+
+
+
+
